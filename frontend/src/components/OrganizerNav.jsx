6ad7f08dd684968adf0c3fa91847e38663ba9c28
Gerlyn Tan <gerlyntan07@gmail.com>
2025-08-02 10:00:05 +0800

    Improve OrganizerNav and OrganizerEvent style

--- frontend/src/components/OrganizerNav.jsx
+++ frontend/src/components/OrganizerNav.jsx
@@ -3,21 +3,45 @@ import { IoExitOutline } from "react-icons/io5";
 import { IoIosArrowBack } from "react-icons/io";
 import { FiMenu, FiX } from "react-icons/fi";
 import { CgProfile } from "react-icons/cg";
+import { Link, useLocation } from "react-router-dom";
 
 function OrganizerNav() {
   const [isOpen, setIsOpen] = useState(false);
+  const location = useLocation();
+  const currentPath = location.pathname;
+
+  let title;
+
+  switch (currentPath) {
+    case "/organizer-dashboard":
+      document.title = 'Dashboard | Sari-Sari Events';
+      title = 'Dashboard Overview';
+      break;
+    case "/my-event":
+      document.title = 'My Events | Sari-Sari Events';
+      title = 'Event Overview';
+      break;
+    default:
+      document.title = 'Sari-Sari Events';
+      title = <h1>404 - Page Not Found</h1>;
+  }
+
 
   return (
     <>
       {/* Mobile Top Navbar */}
-      <div className="md:hidden fixed top-0 left-0 w-full h-16 bg-white z-50 px-4 flex items-center justify-between shadow">
+      <div className="md:hidden fixed top-0 left-0 w-full h-16 bg-white z-50 px-4 flex flex-row items-center justify-between shadow">
         {/* Burger Menu */}
-        <button
-          onClick={() => setIsOpen(true)}
-          className="text-2xl text-gray-700 cursor-pointer"
-        >
-          <FiMenu />
-        </button>
+        <div className="flex flex-row gap-5 items-center">
+          <button
+            onClick={() => setIsOpen(true)}
+            className="text-2xl text-gray-700 cursor-pointer"
+          >
+            <FiMenu />
+          </button>
+
+          <p className="font-outfit text-lg">{title}</p>
+        </div>
 
         {/* Profile Icon */}
         <CgProfile className="text-4xl sm:text-5xl text-gray-600" />
@@ -43,13 +67,21 @@ function OrganizerNav() {
 
             {/* Navigation */}
             <div className="flex flex-col items-center justify-center">
-            <ul className="space-y-5 pl-10 w-full text-center text-gray-800 flex flex-col items-start">
-              <li className="cursor-pointer hover:text-teal-600 transition-colors">Dashboard</li>
-              <li className="cursor-pointer hover:text-teal-600 transition-colors">My Events</li>
-              <li className="cursor-pointer hover:text-teal-600 transition-colors">Attendees</li>
-              <li className="cursor-pointer hover:text-teal-600 transition-colors">Manage Account</li>
-            </ul>
-            </div>            
+              <ul className="w-full text-center bg-white text-gray-800 flex flex-col items-start">
+                <Link to='/organizer-dashboard' className={`${title === 'Dashboard Overview' && `bg-[#009494]`} w-full text-start cursor-pointer hover:text-teal-600 transition-colors`}>
+                  <p className={`${title === 'Dashboard Overview' && `bg-[#EEEEEE]`} py-3 ml-5 pl-3 font-outfit`}>Dashboard</p>
+                </Link>
+                <Link to='/my-event' className={`${title === 'Event Overview' && `bg-[#5BD4D4]`} w-full text-start cursor-pointer hover:text-teal-600 transition-colors`}>
+                  <p className={`${title === 'Event Overview' && `bg-[#EEEEEE]`} py-3 ml-5 pl-3 font-outfit`}>My Event</p>
+                </Link>
+                <Link to='/my-event' className={`${title === 'Attendees Overview' && `bg-[#FF965D]`} w-full text-start cursor-pointer hover:text-teal-600 transition-colors`}>
+                  <p className={`${title === 'Attendees Overview' && `bg-[#EEEEEE]`} py-3 ml-5 pl-3 font-outfit`}>Attendees</p>
+                </Link>
+                <Link to='/my-event' className={`${title === 'Manage Account' && `bg-[#EF4B4C]`} w-full text-start cursor-pointer hover:text-teal-600 transition-colors`}>
+                  <p className={`${title === 'Manage Account' && `bg-[#EEEEEE]`} py-3 ml-5 pl-3 font-outfit`}>Manage Account</p>
+                </Link>
+              </ul>
+            </div>
 
             <hr className="my-5 border-gray-300" />
 
@@ -81,14 +113,70 @@ function OrganizerNav() {
           <img src="/sariLogo.png" alt="Sari-Sari Events Logo" className="h-10" />
         </div>
 
-
         {/* Navigation */}
-        <ul className="space-y-5 text-left px-10 text-gray-800">
-          <li className="cursor-pointer hover:text-teal-600 transition-colors">Dashboard</li>
-          <li className="cursor-pointer hover:text-teal-600 transition-colors">My Events</li>
-          <li className="cursor-pointer hover:text-teal-600 transition-colors">Attendees</li>
-          <li className="cursor-pointer hover:text-teal-600 transition-colors">Manage Account</li>
-        </ul>
+        <div className="flex flex-col items-center justify-center w-full bg-gray-200">
+      <ul className="w-full text-center bg-white text-gray-800 flex flex-col items-start">
+        <Link
+          to="/organizer-dashboard"
+          className={`${
+            title === 'Dashboard Overview' ? 'bg-[#009494]' : ''
+          } w-full text-start cursor-pointer hover:text-teal-600 transition-colors`}
+        >
+          <p
+            className={`${
+              title === 'Dashboard Overview' ? 'bg-[#EEEEEE]' : ''
+            } py-3 ml-5 pl-3 font-outfit`}
+          >
+            Dashboard
+          </p>
+        </Link>
+
+        <Link
+          to="/my-event"
+          className={`${
+            title === 'Event Overview' ? 'bg-[#5BD4D4]' : ''
+          } w-full text-start cursor-pointer hover:text-teal-600 transition-colors`}
+        >
+          <p
+            className={`${
+              title === 'Event Overview' ? 'bg-[#EEEEEE]' : ''
+            } py-3 ml-5 pl-3 font-outfit`}
+          >
+            My Events
+          </p>
+        </Link>
+
+        <Link
+          to="/attendees"
+          className={`${
+            title === 'Attendees Overview' ? 'bg-[#FF965D]' : ''
+          } w-full text-start cursor-pointer hover:text-teal-600 transition-colors`}
+        >
+          <p
+            className={`${
+              title === 'Attendees Overview' ? 'bg-[#EEEEEE]' : ''
+            } py-3 ml-5 pl-3 font-outfit`}
+          >
+            Attendees
+          </p>
+        </Link>
+
+        <Link
+          to="/manage-account"
+          className={`${
+            title === 'Manage Account' ? 'bg-[#EF4B4C]' : ''
+          } w-full text-start cursor-pointer hover:text-teal-600 transition-colors`}
+        >
+          <p
+            className={`${
+              title === 'Manage Account' ? 'bg-[#EEEEEE]' : ''
+            } py-3 ml-5 pl-3 font-outfit`}
+          >
+            Manage Account
+          </p>
+        </Link>
+      </ul>
+    </div>
 
 
 
--- frontend/src/pages/OrganizerEvent.jsx
+++ frontend/src/pages/OrganizerEvent.jsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, {useState} from 'react';
 import OrganizerNav from '../components/OrganizerNav';
 import EventCard from '../components/OrganizerEventCard';
 
 const OrganizerEvent = () => {
+  const [selectedCategory, setSelectedCategory] = useState('All Events');
   const events = [
     {
       name: 'Tech Conference 2023',
@@ -24,27 +25,28 @@ const OrganizerEvent = () => {
     },
   ];
 
+
   return (
     <div className="bg-gray-50 min-h-screen font-sans">
       <OrganizerNav />
 
       {/* Content Container */}
-      <div className="pt-20 md:ml-64 p-4 md:p-8 lg:p-12 flex flex-col items-center">
+      <div className="pt-23 md:ml-64 p-4 md:p-8 lg:p-12 flex flex-col items-center">
         {/* Header */}
-        <div className="flex justify-center mb-8">
+        <div className="justify-center mb-5 hidden md:flex">
           <h1 className="text-2xl sm:text-3xl md:text-4xl font-light font-outfit text-gray-800">
-            My Events
+            Event Overview
           </h1>
         </div>
 
         {/* Search & Buttons */}
-        <div className="flex flex-col md:flex-row items-center justify-between gap-4 mb-8">
+        <div className="flex flex-col w-[90%] md:w-full items-center md:items-start justify-between gap-4 mb-8">
           {/* Search bar */}
-          <div className="relative w-full md:w-1/2">
+          <div className="relative w-full">
             <input
               type="text"
               placeholder="Search..."
-              className="w-full pl-10 pr-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-teal-500 transition"
+              className="w-full pl-10 pr-4 py-2 border-b-2 border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-teal-500 transition"
             />
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -63,18 +65,26 @@ const OrganizerEvent = () => {
           </div>
 
           {/* Action buttons */}
-          <div className="flex flex-col md:flex-row gap-55 w-full md:w-auto">
-            <button className="px-6 py-2 bg-secondary text-white rounded-full hover:bg-secondary/80 font-outfit shadow">
+          <div className="flex flex-row w-full gap-3 items-center lg:w-[80%] xl:w-[70%] lg:justify-between overflow-x-auto hide-scrollbar text-sm">
+            <button className={`${selectedCategory === 'All Events' ? `bg-secondary text-white` : `border-1 border-secondary text-secondary`} whitespace-nowrap px-4 lg:px-7 xl:px-10 py-2 rounded-full hover:bg-secondary/80 hover:text-white font-outfit shadow cursor-pointer`}>
               All Events
             </button>
-
-            <button className="px-6 py-2 bg-secondary text-white rounded-lg hover:bg-secondary/80 font-outfit shadow">
-              Create Event
+            <button className={`${selectedCategory === 'Upcoming' ? `bg-secondary text-white` : `border-1 border-secondary text-secondary`} whitespace-nowrap px-4 lg:px-7 xl:px-10 py-2 rounded-full hover:bg-secondary/80 hover:text-white font-outfit shadow cursor-pointer`}>
+              Upcoming
+            </button>
+            <button className={`${selectedCategory === 'Ongoing' ? `bg-secondary text-white` : `border-1 border-secondary text-secondary`} whitespace-nowrap px-4 lg:px-7 xl:px-10 py-2 rounded-full hover:bg-secondary/80 hover:text-white font-outfit shadow cursor-pointer`}>
+              Ongoing
+            </button>
+            <button className={`${selectedCategory === 'Recent' ? `bg-secondary text-white` : `border-1 border-secondary text-secondary`} whitespace-nowrap px-4 lg:px-7 xl:px-10 py-2 rounded-full hover:bg-secondary/80 hover:text-white font-outfit shadow cursor-pointer`}>
+              Recent
             </button>
           </div>
+
+          <button className='bg-secondary text-white mt-8 w-full py-3 rounded-lg font-outfit md:self-start md:w-auto md:px-5 cursor-pointer hover:bg-secondary/80 hover:text-white'>Create New Event</button>
         </div>
 
-        <div className='grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-3 xl:gap-5 mt-8 w-[95%] lg:w-full'>
+
+        <div className='grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-3 lg:gap-8 w-[95%] lg:w-full'>
           {events.map((event, i) => (
             <EventCard
               key={i}
